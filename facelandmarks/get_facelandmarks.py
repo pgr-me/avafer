fc77566480ce9bc252b2cc3dea562cac278c5d0c
Peter Rasmussen <pgr-me@protonmail.com>
2024-04-27 18:10:26 +0000

    Update facelandmarks code to work with pipeline.

--- facelandmarks/get_facelandmarks.py
+++ facelandmarks/get_facelandmarks.py
@@ -4,41 +4,65 @@ import argparse
 from pathlib import Path
 import sys
 # Third party imports
+from loguru import logger
 import numpy as np
 from PIL import Image
 import torch
+from torchvision.datasets import ImageFolder
+from tqdm import tqdm
 # Local imports
 from src import detect_faces
 
 
-SRC_DIR = Path("/workspace/data/raw")
-DST_DIR = Path("/workspace/data/processed")
-IM_SUFFIXES = (".jpg", ".jpeg", ".png", ".tif", ".tiff",)
+# IO defaults
+ROOT_DIR = Path("/data/")
+BENCHMARK = "test_dataset"
+SUPPORTED_BENCHMARKS = ("fer2013", "test_dataset",)  # test_datset is just a sampling of fer2013
+SUFFIXES = (".jpg", ".jpeg", ".png", ".tif", ".tiff",)
+
 
 def argparser():
     parser_ = argparse.ArgumentParser()
-    i_help = "Path to directory of images to compute five facial landmarks for."
-    o_help = "Path to output directory that provides a text file for each input image."
-    parser_.add_argument("-i", "--src_dir", type=Path, default=SRC_DIR, help=i_help)
-    parser_.add_argument("-o", "--dst_dir", type=Path, default=DST_DIR, help=o_help)
+    # IO arguments
+    # TODO: Add specifics on root directory's construction
+    io_help = "Path to root data directory; refer to repository-level README for specifics."
+    bm_help = "Name of benchmark dataset to process."
+    parser_.add_argument("-io", "--root_dir", type=Path, default=ROOT_DIR, help=io_help)
+    parser_.add_argument("-bm", "--benchmark", type=str, default=BENCHMARK, help=bm_help)
     return parser_.parse_args()
 
   
 if __name__ == "__main__":
     args = argparser()
-    assert args.src_dir.exists(), f"{args.src_dir} doesn't exist."
-    srcs = sorted([x for x in args.src_dir.iterdir() if x.suffix in IM_SUFFIXES])
-    if len(srcs) == 0:
-        print(f"No files to process in {args.src_dir}.")
+    logger.add("facelandmarks.log", rotation="1 week")
+    logger.info("Arguments:")
+    for k, v in vars(args).items():
+        logger.debug(f"{k}: {v}")
+    if args.benchmark in SUPPORTED_BENCHMARKS:
+        benchmark_dir = args.root_dir / args.benchmark
+        src_dir = benchmark_dir / "facerestore"
+        im_folder = ImageFolder(src_dir)
+        srcs = [Path(x[0]) for x in im_folder.imgs]
+        im_folder = ImageFolder(src_dir)
+        dst_dir = benchmark_dir / "facelandmarks"
+    else:
+        logger.error(f"Benchmark {args.benchmark} is not supported")
         sys.exit(1)
-    args.dst_dir.mkdir(exist_ok=True, parents=True)
-    for src in srcs:
+    srcs = [x for x in srcs if x.suffix in SUFFIXES]
+    if len(srcs) == 0:
+        logger.error(f"No files to process in {args.src_dir}.")
+        sys.exit(1)
+    dst_dir.mkdir(exist_ok=True, parents=True)
+    for src in tqdm(srcs):
         image = Image.open(src)
         with torch.no_grad():
             bb, lm = detect_faces(image)
-        lm = lm[0].reshape((2, 5)).T
-        dst = args.dst_dir / f"{src.stem}.txt"
+        try:
+            lm = lm[0].reshape((2, 5)).T
+        except:
+            import ipdb; ipdb.set_trace()
+        emo_subdir = dst_dir / src.parents[0].name
+        emo_subdir.mkdir(exist_ok=True, parents=True)
+        dst = emo_subdir / f"{src.stem}.png"
         np.savetxt(dst, lm, fmt="%.2f")
 
-
-    pass
