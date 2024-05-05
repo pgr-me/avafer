babd6e3fe54e93f4fb29c1f11b3a84fee22edf18
Peter Rasmussen <pgr-me@protonmail.com>
2024-05-05 22:57:14 +0000

    Make deoldify the second step, after super res

--- facerestore/facerestore_deoldify.py
+++ facerestore/facerestore_deoldify.py
@@ -25,7 +25,8 @@ torch.backends.cudnn.benchmark = True
 
 # IO defaults
 BENCHMARK = "test_dataset"
-STEP = "DEOLDIFY"  # HuggingFace colorization and super resolution used in this script
+PRED_STEP = "facerestore_resshift"  # Predecessor step to use; if None then this is first step
+STEP = "facerestore_deoldify"  # HuggingFace colorization and super resolution used in this script
 ROOT_DIR = Path("/data")
 SUFFIXES = (".png", ".jpg", ".jpeg", ".tif", ".tiff")
 SUPPORTED_BENCHMARKS = (
@@ -46,10 +47,12 @@ def argparser():
     # TODO: Add specifics on root directory's construction
     bm_help = "Name of benchmark dataset to process."
     io_help = "Path to root data directory; refer to README for specifics on that directory's construction."
-    na_help = "Name of destination directory (e.g., HF for HuggingFace routine in this script)."
+    ps_help = "Name of predecessor step corresponding to sub-directory in data/benchmark dir."
+    st_help = "Name of destination directory (e.g., HF for HuggingFace routine in this script)."
     parser.add_argument("-bm", "--benchmark", default=BENCHMARK, type=str, help=bm_help)
     parser.add_argument("-io", "--root_dir", default=ROOT_DIR, type=Path, help=io_help)
-    parser.add_argument("-st", "--step", default=STEP, type=str, help=na_help)
+    parser.add_argument("-ps", "--pred_step", default=PRED_STEP, help=ps_help)
+    parser.add_argument("-st", "--step", default=STEP, type=str, help=st_help)
     # Colorizer arguments
     rf_help = "Lower number is more colorful but more likely to hallucinate."
     parser.add_argument("-rf", "--render_factor", type=int, default=RENDER_FACTOR, help=rf_help)
@@ -66,19 +69,21 @@ def colorize(img: Image, device: str, model) -> Image:
     return Image.fromarray((output_arr * 255).round().astype(np.uint8))
 
 
-
 if __name__ == "__main__":
     args = argparser()
+    logger.add("facerestore_deoldify.log", rotation="1 week")
+    logger.info(f"Arguments")
     for k, v in vars(args).items():
-        logger.debug(f"{k:>20s}:{v}")
+        logger.debug(f"{k}: {v}")
     if args.benchmark in SUPPORTED_BENCHMARKS:
         benchmark_dir = args.root_dir / args.benchmark
-        src_dir = (
-            benchmark_dir / "raw" / "test"
-        )  # We only use test data for experimentation
-        im_folder = ImageFolder(src_dir)
+        if args.pred_step is None:
+            pred_dir = benchmark_dir / "raw" / "test"
+        else:
+            pred_dir = benchmark_dir / args.pred_step
+        im_folder = ImageFolder(pred_dir)
         srcs = [Path(x[0]) for x in im_folder.imgs]
-        dst_dir = benchmark_dir / "facerestore" / args.step
+        dst_dir = benchmark_dir / args.step
     else:
         raise NotImplementedError(f"Benchmark {args.benchmark} is not supported.")
     srcs = [x for x in srcs if x.suffix in SUFFIXES]
@@ -87,16 +92,17 @@ if __name__ == "__main__":
         sys.exit(1)
     dst_dir.mkdir(exist_ok=True, parents=True)
 
-    # Load colorization model and send to device
+    logger.info("Load colorization model and send to device.")
     learn = gen_inference_wide(root_folder=Path("./"), weights_name="ColorizeStable_gen")
     colorizer_filter = ColorizerFilter(learn)
 
     logger.info(f"Colorize images.")
     for src in tqdm(srcs):
-        emo_subdir = dst_dir / src.parents[0].name
+        emo = src.parents[0].name
+        emo_subdir = dst_dir / emo
         emo_subdir.mkdir(exist_ok=True, parents=True)
         dst = emo_subdir / f"{src.stem}.png"
         src_im = Image.open(src).convert("RGB")
         colorized_im = colorizer_filter.filter(src_im, src_im, args.render_factor, post_process=True)
         colorized_im.save(dst)
-    loguru.info(f"Finish DeOldify colorization.")
+    logger.info(f"Finish DeOldify colorization.")
